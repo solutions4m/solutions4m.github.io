c01fd79af7ec0cd037301087718a70b71dd00c2f
ich <mathias.herrmann@sig-ulm.de>
2017-08-31 20:44:01 +0700

    Initial commit

---
+++ wp-admin/widgets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Weiterleiten&#160;&hellip;</title>
+		<meta http-equiv="refresh" content="0;url=https://solutions4m.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://solutions4m.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1";
+		</script>
+
+		<p>Du wirst auf <a href="https://solutions4m.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1">https://solutions4m.github.io/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fwidgets.php&reauth=1</a> weitergeleitet</p>
+	</body>
+</html>
